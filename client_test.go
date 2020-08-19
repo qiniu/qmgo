560f90c4cc013aa559c881522c5bb735be6d9caf
jiangzhi <jiangzhi@qiniu.com>
2020-08-19 21:03:00 +0800

    support auth when connect

--- client_test.go
+++ client_test.go
@@ -63,6 +63,7 @@ func TestQmgoClient(t *testing.T) {
 		MaxPoolSize:      &maxPoolSize,
 		MinPoolSize:      &minPoolSize,
 		ReadPreference:   &ReadPref{Mode: readpref.SecondaryMode, MaxStalenessMS: 500},
+		Auth:             &Credential{Username: "admin", Password: "password"},
 	}
 
 	cli, err := Open(context.Background(), &cfg)
@@ -148,4 +149,22 @@ func TestClient_ServerVersion(t *testing.T) {
 	version := cli.ServerVersion()
 	ast.NotEmpty(version)
 	fmt.Println(version)
+}
+
+func TestClient_newAuth(t *testing.T) {
+	ast := require.New(t)
+
+	auth := Credential{
+		AuthMechanism: "PLAIN",
+		AuthSource:    "PLAIN",
+		Username:      "qmgo",
+		Password:      "123",
+		PasswordSet:   false,
+	}
+	cred := newAuth(auth)
+	ast.Equal(auth.PasswordSet, cred.PasswordSet)
+	ast.Equal(auth.AuthSource, cred.AuthSource)
+	ast.Equal(auth.AuthMechanism, cred.AuthMechanism)
+	ast.Equal(auth.Username, cred.Username)
+	ast.Equal(auth.Password, cred.Password)
 }
