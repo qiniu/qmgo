d22c74cc85cfbe90bfc75c3c2b6a0bef0b6dacf0
jiangzhi <jiangzhi@qiniu.com>
2020-08-22 23:16:40 +0800

    test traivs

--- collection_test.go
+++ collection_test.go
@@ -9,12 +9,12 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-var cli = initClient("test")
+//var cli = initClient("test")
 
 func TestCollection_EnsureIndex(t *testing.T) {
 	ast := require.New(t)
-	// cli := initClient("test")
-	// defer cli.Close(context.Background())
+	cli := initClient("test")
+	defer cli.Close(context.Backgrounds())
 	defer cli.DropCollection(context.Background())
 
 	cli.ensureIndex(context.Background(), nil, false)
